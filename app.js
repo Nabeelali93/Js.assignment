391b711645d2c75c6a3edc16acbca17ba25683cd
Nabeel <Nabeealali720@gmail.com>
2022-10-31 13:16:22 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,143 @@
+
+// Chapter 1
+// tast no 1
+
+// alert("Error Please enter a valid password")
+
+// tast no 2
+
+// alert("Werlcome to Js Land \n Happy coding")
+
+// tast no 3
+
+// alert("Werlcome to Js Land...")
+
+// tast no 4
+
+// alert("Hell0 i can run JS through my web browser's console ")
+
+// tast no 5
+
+// alert("my exiting alert in html")
+
+
+// chapter 2
+
+// task no 1
+
+// var username
+
+// task no 2
+
+// var myname ="Jhon doe"
+
+
+// task no 3
+
+// var message
+// message ="Hello world"
+
+// alert(message)
+
+// task no 4
+
+// var std_name = "Aman"
+// var std_Age = 15
+// var course = "Certified Mobile app and web development"
+
+// alert(std_name)
+// alert(std_Age)
+// alert(course)
+
+// task no 5
+
+// var  pizza = "pizza \n pizz \n piz \n pi \n P "
+
+// alert(pizza)
+
+// task no 6
+
+// var email = "Aman@gmail.com"
+// alert("My email address is" +"  "+ email)
+
+
+// task no 7
+
+
+// var book = "A smarter \n way to learn JavaScript"
+
+// alert("i'm trying to learn from the book"+ book)
+
+// task no 8
+
+// document.write("<p>yah! i can write HTML content through JavaScript</p>")
+
+// task no 9
+
+// var ex = "------------ஜ۩۞۩ஜ--------------"
+
+// alert(ex)
+
+// chapter 3
+
+// task no 1
+
+// var age = 27
+// alert("i am" + age +" years old" )
+
+// task no 2
+
+// var vist = 10
+
+// var vist = ++vist
+
+// alert("You have visited this site "+vist+"times")
+
+
+
+// task no 3
+
+// var by = parseInt(prompt("Enter Your birth age"))
+
+// document.write("My birth year is"+" "+by)
+
+// document.write(" <br>data type of  my decleared veriable is "+typeof by )
+
+// task no 4
+
+// var visiter = "Jhon Doe"
+
+// var prodact_title= "T-Shirt"
+
+// var qty = 5
+
+// document.write(" <b>"+visiter+"</b> "+ "ordered "+ " <b>"+qty+"</b> "+" <b>"+prodact_title+"</b>"+"(s)"+"on XYZ clothing store"  )
+
+// chapter no 04
+
+// task no 01
+// var name = "Asad" , age=23 , number =03431231233;
+
+// task no 02
+
+// var Aman 
+// var _wasi
+// var $world 
+// var alione 
+// var $_321pill
+
+
+// var 12dua 
+// var &ali
+// var *xyz 
+// var @ghs
+// var <world
+
+// task no 03
+
+document.write("<h1>Rules for naming JS variables</h1>" )
+
+document.write("<p> Variable names can only contain number, alphabat,$ and _.example $my_1stVariable </p>")
+document.write("<p>Variables must begin with a latter,$ or_. For example  $name, _name or name</p>")
+document.write("<p> Variable names are case sensitive</p>")
+document.write("<p>Variable names should not be JS Kyewords</p>")
